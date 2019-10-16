d2e9d06e7d03c233fd6cf0790e438e744f20b514
Ramon Correa <ramon@Ashishs-MBP.attlocal.net>
2019-10-16 02:28:41 -0400

    let's do it dicretely hehehehe

--- hw3.py
+++ hw3.py
@@ -4,72 +4,76 @@ import math
 import scipy.stats as sstats
 import time 
 class decisionTree: 
-    def __init__(self,x,y,cost=None,depth=5):  
+    def __init__(self,x,y,cost=None,depth=2):  
         if depth ==0:  
             #handle case where we've reached the bottom 
-            self.decision = sstats.mode(y) 
-            self.left =None 
-            self.right =None 
+            self.decision = sstats.mode(y)  
+            self.depth =0 
+            self.children = None 
+        self.depth =depth
+        self.decision = None 
+        self.children =list() 
         #case where we are not the bottom and need to split 
         (cost,f1,f2,f1_val,f2_val) = self.get_best(x,y) 
-        breakpoint() 
-        print(f"With Cost we have obtained obtimal split to be ")
+        #set up decision variables 
+        self.feat_1 = f1 
+        self.feat_1_thresh = f1_val 
+        self.feat_2 = f2 
+        self.feat_2_thresh = f2_val 
+        l_f1 = x[:,f1] <= f1_val 
+        l_f2 = x[:,f2] <= f2_val 
+        g_f1 =  np.logical_not(l_f1)
+        g_f2 = np.logical_not(l_f2) 
+        buckets = [ np.logical_and(l_f1,l_f2),np.logical_and(l_f1,g_f2),np.logical_and(g_f1,l_f2),np.logical_and(g_f1,g_f2)]
+        for b in buckets: 
+            self.children.append(decisionTree(x[b],y[b] ,depth=self.depth-1))
     def fit(self,X,y,cost='log',depth=5): 
         print("hi") 
-    def generate_unique_values(self,x,y):
+    def generate_unique_values(self,x):
+        """ Instead of computing thresholds each time let's pre compute them 
+        x: our feature matrix 
+        """
         unique_list = list() 
         for i in range( x.shape[1]):
-            unique_list.append( self.inbetween_vals(x[:,i],y))  
+            unique_list.append( self.inbetween_vals(x[:,i]))  
         return unique_list
     def get_best(self,X,Y):  
-        t = time.time() 
         combi = combinations(range(X.shape[1]),2) 
         my_list = list()  
         counter = 0 
         best_tuple = (-1*np.inf,0,0) 
         best_pair = (0,0)
-        unique_list = self.generate_unique_values(X,Y)
-        i =0
+        unique_list = self.generate_unique_values(X)
         for f1,f2 in combi:  
             current_cost  = self.eval_metrix(X[:,[f1,f2]],Y,unique_list[f1],unique_list[f2])  #shape of currenct_cos (c_cost,f1_t,f2_t) 
-            print(f"finished {i} one combination after {time.time() -t}")
+            #print(f"finished {i} one combination after {time.time() -t}")
             if current_cost[0] > best_tuple[0]:
                 best_tuple = current_cost 
                 best_pair = (f1,f2) 
-            i = i +1 
-        return  (best_tuple[0],best_pair[0],best_pair[1],best_tuple[0],best_tuple[1])
+        return  (best_tuple[0],best_pair[0],best_pair[1],best_tuple[1],best_tuple[2])
     def eval_metrix(self,x,y,f1_unique,f2_unique): 
         t = time.time() 
-        #self.inspect_x(x,y)
         my_list = list() 
         feat_pairs = list(product(f1_unique,f2_unique)  ) #let's look at every combination  of these feature values    
         best_tuple = (-1*np.inf,0,0)
-        #print(f"Starting to evaluate feature pairs: {time.time() - t}")
         for f1,f2 in feat_pairs: 
             current_cost = self.eval_cost(x,y,f1,f2) # shape of current loss (LL,f1_val,f2_val) 
             if current_cost[0] > best_tuple[0] :
                 best_tuple = current_cost  
-        #jprint(f"Finished evaluating feature pairs: {time.time() - t } ") 
-        #print(f"Total elements was {len(feat_pairs)}")
         return best_tuple
-    def inbetween_vals(self,whole_x,y):  
-        c1_peak = np.mean(whole_x[y==1] ) 
-        c2_peak = np.mean(whole_x[y!=1])  
-        x = np.unique(whole_x)
-        if c1_peak > c2_peak: 
-            #c1 peak is the greatest 
-            potential_vals = x[ np.logical_and( x>=c2_peak , x <=c1_peak)]
-        else: 
-            potential_vals = x[ np.logical_and( x>=c1_peak , x<=c2_peak) ] 
+    def inbetween_vals(self,whole_x): 
+        """ inbetween_vals generates thresholds based on middle point between unique values  
+        whole_x is entire column vector of feature values 
+        y is the albel 
+        """ 
+        potential_vals = np.unique(whole_x)
         inbetween = list()   
         for i in range(len(potential_vals) -1): 
             inbetween.append( (potential_vals[i] + potential_vals[i+1] )/2)   
-        if len(inbetween ) == 0 and len(potential_vals) >0:
-            inbetween.append(potential_vals[0])
         return inbetween
 
     def eval_cost(self,x,y,f1_val,f2_val):
-        """ since this is a way decision problem. we can no longer asign onse side is negativ eone side is positive. 
+        """ since this is a 4-way decision problem. we can no longer asign onse side is negativ eone side is positive. 
             we may wish to utilize the  most common class in each of the corresponding buckets as our prediciton label 
             this would lead us to the potential if lacking proper class splits producing a null classifier
         """
@@ -95,11 +99,8 @@ class decisionTree:
             return correct*np.log(p)  + incorrect*np.log(1-p)
         else:  
             return 0 
-        #need an if statement for being a negative node or a positive node 
-        #this would be performnace for a single child node  so it would bea column of the decision table 
-        #you would need a filtrer opteratio nusign the logicla labels to get only the labels of intetest
-        # calcualte true positives, true negatives and so forth 
-    def get_common_label(self,y):  
+    def get_common_label(self,y):   
+        #no longer used funciton that was meant to get the mode of my labels 
         u_labels = np.unique(y)
         class_counts = np.zeros((len(u_labels),2))  
         for i,label in enumerate(u_labels): 
--- problem1.py
+++ problem1.py
@@ -1,11 +1,16 @@
 from sklearn import datasets  
 from hw3 import decisionTree
+import pandas as pd 
 import cProfile
 import numpy as np 
 
-def binner(x,k=10): 
-
+def binner(x,k=10):  
+    for i in range(x.shape[1]): 
+        w,b= pd.cut(x[:,i],bins=k,labels=False,retbins=True)  
+        x[:,i] = w 
 data = datasets.load_breast_cancer()
-(x,y) = (data.data,data.target) 
-bi
-#model = decisionTree(np.round(x),y) 
+(x,y) = (data.data,data.target)  
+binner(x,k=10)
+model = decisionTree(x,y)  
+breakpoint() 
+print("Hi")
