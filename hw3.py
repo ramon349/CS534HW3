5891c999b4ba1a960cd0fe85135ed41b2d5a4b9a
Ramon Correa <ramon@ashishs-mbp.wireless.emory.edu>
2019-10-16 17:56:24 -0400

    final version

--- hw3.py
+++ hw3.py
@@ -5,30 +5,46 @@ import scipy.stats as sstats
 import time 
 class decisionTree: 
     def __init__(self,x,y,cost=None,depth=2):  
-        if depth ==0:  
+        if depth ==1:  
             #handle case where we've reached the bottom 
-            self.decision = sstats.mode(y)  
-            self.depth =0 
+            self.decision = sstats.mode(y).mode
+            self.depth =0
             self.children = None 
-        self.depth =depth
-        self.decision = None 
-        self.children =list() 
-        #case where we are not the bottom and need to split 
-        (cost,f1,f2,f1_val,f2_val) = self.get_best(x,y) 
-        #set up decision variables 
-        self.feat_1 = f1 
-        self.feat_1_thresh = f1_val 
-        self.feat_2 = f2 
-        self.feat_2_thresh = f2_val 
-        l_f1 = x[:,f1] <= f1_val 
-        l_f2 = x[:,f2] <= f2_val 
-        g_f1 =  np.logical_not(l_f1)
-        g_f2 = np.logical_not(l_f2) 
-        buckets = [ np.logical_and(l_f1,l_f2),np.logical_and(l_f1,g_f2),np.logical_and(g_f1,l_f2),np.logical_and(g_f1,g_f2)]
-        for b in buckets: 
-            self.children.append(decisionTree(x[b],y[b] ,depth=self.depth-1))
-    def fit(self,X,y,cost='log',depth=5): 
-        print("hi") 
+        else: 
+            self.depth =depth
+            self.decision = None 
+            self.children =list() 
+            #case where we are not the bottom and need to split 
+            (cost,f1,f2,f1_val,f2_val) = self.get_best(x,y) 
+            print(f"With cost {cost} we found f1: {f1} t: {f1_val} and f2:{f2} t:{f2_val}")
+            #set up decision variables 
+            self.feat_1 = f1 
+            self.feat_1_thresh = f1_val 
+            self.feat_2 = f2 
+            self.feat_2_thresh = f2_val 
+            l_f1 = x[:,f1] < f1_val 
+            l_f2 = x[:,f2] < f2_val 
+            g_f1 =  np.logical_not(l_f1)
+            g_f2 = np.logical_not(l_f2) 
+            buckets = [ np.logical_and(l_f1,l_f2),np.logical_and(l_f1,g_f2),np.logical_and(g_f1,l_f2),np.logical_and(g_f1,g_f2)]
+            for b in buckets: 
+                self.children.append(decisionTree(x[b],y[b] ,depth=self.depth-1))
+    def fit(self,X,y,cost='log',depth=5):  
+        print("hi")  
+    def predict(self,x):
+        if self.decision != None:
+            return self.decision 
+        else: 
+            l_f1 = x[self.feat_1] <= self.feat_1_thresh
+            l_f2 = x[self.feat_2] <= self.feat_2_thresh
+            g_f1 =  np.logical_not(l_f1)
+            g_f2 = np.logical_not(l_f2) 
+            buckets = [ np.logical_and(l_f1,l_f2),np.logical_and(l_f1,g_f2),np.logical_and(g_f1,l_f2),np.logical_and(g_f1,g_f2)] 
+            if np.sum(buckets)>1:
+                breakpoint()
+            for i,b in enumerate(buckets): 
+                if b:
+                    return  self.children[i].predict(x)
     def generate_unique_values(self,x):
         """ Instead of computing thresholds each time let's pre compute them 
         x: our feature matrix 
@@ -39,21 +55,16 @@ class decisionTree:
         return unique_list
     def get_best(self,X,Y):  
         combi = combinations(range(X.shape[1]),2) 
-        my_list = list()  
-        counter = 0 
         best_tuple = (-1*np.inf,0,0) 
         best_pair = (0,0)
         unique_list = self.generate_unique_values(X)
         for f1,f2 in combi:  
             current_cost  = self.eval_metrix(X[:,[f1,f2]],Y,unique_list[f1],unique_list[f2])  #shape of currenct_cos (c_cost,f1_t,f2_t) 
-            #print(f"finished {i} one combination after {time.time() -t}")
             if current_cost[0] > best_tuple[0]:
                 best_tuple = current_cost 
                 best_pair = (f1,f2) 
         return  (best_tuple[0],best_pair[0],best_pair[1],best_tuple[1],best_tuple[2])
     def eval_metrix(self,x,y,f1_unique,f2_unique): 
-        t = time.time() 
-        my_list = list() 
         feat_pairs = list(product(f1_unique,f2_unique)  ) #let's look at every combination  of these feature values    
         best_tuple = (-1*np.inf,0,0)
         for f1,f2 in feat_pairs: 
@@ -66,10 +77,13 @@ class decisionTree:
         whole_x is entire column vector of feature values 
         y is the albel 
         """ 
-        potential_vals = np.unique(whole_x)
+        potential_vals = np.unique(whole_x) 
         inbetween = list()   
+        inbetween = potential_vals
+        """
         for i in range(len(potential_vals) -1): 
             inbetween.append( (potential_vals[i] + potential_vals[i+1] )/2)   
+        """ 
         return inbetween
 
     def eval_cost(self,x,y,f1_val,f2_val):
@@ -92,13 +106,19 @@ class decisionTree:
             return (LL,f1_val,f2_val)
     def get_bucket_performance(self,real_labels):  
         if real_labels.size != 0: 
-            common_class = sstats.mode(real_labels)  # self.get_common_label(real_labels) 
-            correct = np.sum(real_labels == common_class)
-            incorrect = real_labels.size - correct # instead of redoign the sum lets just get the difference np.sum(real_labels != common_class) 
+            common_class =1   # self.get_common_label(real_labels) 
+            TP = np.sum( real_labels == common_class) 
             p = np.mean(real_labels)
-            return correct*np.log(p)  + incorrect*np.log(1-p)
-        else:  
-            return 0 
+            FP = np.sum(real_labels != common_class)   
+            out1 = TP*np.log(p) 
+            out2 = FP*np.log(p)
+            if out1 == -1*math.inf: 
+                out1 = 0  
+            if out2 == -1*math.inf: 
+                out2 = 0 
+            return out1 + out2
+        else:
+            return 0
     def get_common_label(self,y):   
         #no longer used funciton that was meant to get the mode of my labels 
         u_labels = np.unique(y)
--- problem1.py
+++ problem1.py
@@ -2,15 +2,32 @@ from sklearn import datasets
 from hw3 import decisionTree
 import pandas as pd 
 import cProfile
-import numpy as np 
+import numpy as np  
+from sklearn.metrics import  accuracy_score
 
-def binner(x,k=10):  
+def binner(x,k=10):
+    new_x = np.zeros((x.shape[0]))
     for i in range(x.shape[1]): 
-        w,b= pd.cut(x[:,i],bins=k,labels=False,retbins=True)  
-        x[:,i] = w 
+        min_x = np.min(x[:,i]) 
+        max_x = np.max(x[:,i]) 
+        step = (max_x-min_x)/k
+        bins = np.arange(min_x,max_x,step=step)  
+        print(f"lenght of bins {len(bins) }")
+        print(f"{max_x}: {min_x}") 
+        bin_val =1 
+        for j in range(len(bins) -1): 
+            group = np.logical_and(x[:,i] >= bins[j],x[:,i] < bins[j+1] ) 
+            new_x[group] =  bin_val 
+            print(np.sum(group))
+            bin_val +=1 
+        x[:,i] = np.copy(new_x)  
+
 data = datasets.load_breast_cancer()
 (x,y) = (data.data,data.target)  
 binner(x,k=10)
-model = decisionTree(x,y)  
-breakpoint() 
-print("Hi")
+model = decisionTree(x,y,depth=2)   
+predictions = np.zeros((x.shape[0],1) ) 
+for i in range(x.shape[0]):
+    predictions[i] = model.predict(x[i,:]) 
+w = accuracy_score(y,predictions)
+print(w)
