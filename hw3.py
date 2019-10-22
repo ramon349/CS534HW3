7c60b0ffd774023b4b9ef9deb0433767d6807fd6
Ramon Correa <ramon@ashishs-mbp.wireless.emory.edu>
2019-10-21 22:56:52 -0400

    hello

--- hw3.py
+++ hw3.py
@@ -4,7 +4,7 @@ import numpy as np
 import math
 import scipy.stats as sstats
 import time 
-class decisionTree: 
+class QuarternaryDecisionTree: 
     def __init__(self,cost=None,depth=2):
         self.decision = 0 
         self.depth =depth
@@ -21,7 +21,6 @@ class decisionTree:
             self.children =list() 
             #case where we are not the bottom and need to split 
             (cost,f1,f2,f1_val,f2_val) = self.get_best(x,y) 
-            print(f"With cost {cost} we found f1: {f1} t: {f1_val} and f2:{f2} t:{f2_val}")
             #set up decision variables 
             self.feat_1 = f1 
             self.feat_1_thresh = f1_val 
@@ -32,11 +31,18 @@ class decisionTree:
             g_f1 =  np.logical_not(l_f1)
             g_f2 = np.logical_not(l_f2) 
             buckets = [ np.logical_and(l_f1,l_f2),np.logical_and(l_f1,g_f2),np.logical_and(g_f1,l_f2),np.logical_and(g_f1,g_f2)]
-            for b in buckets: 
-                mdl = decisionTree(depth =self.depth-1)  
+            for b in buckets:  
+                mdl = QuarternaryDecisionTree(depth =self.depth-1)  
                 mdl.fit(x[b],y[b],num_sample=self.num_sample)
                 self.children.append(mdl)
     def predict(self,x):
+        """predict 
+        x: 1xm datapoint to be classified 
+        output: 
+            data sets values are analyzed based on nodes descriptive features. 
+            there is an array of logical operations that will determine onto which child node the 
+            sample individual falls under
+        """
         if self.decision != None:
             return self.decision 
         else: 
@@ -57,23 +63,34 @@ class decisionTree:
         for i in range( x.shape[1]):
             unique_list.append( self.inbetween_vals(x[:,i]))  
         return unique_list
-    def get_best(self,X,Y):  
+    def get_best(self,X,Y): 
+        """ 
+            X: nxm data matrix 
+            Y: nx1 data vector consint of labels 
+
+            output: Best feature according to our loss metric as well as the required thresholds 
+            output is organized as 
+            (cost of splitting at node, best feature1,best feature2, threshold_1,threshold2) 
+        """
         combi = combinations(range(X.shape[1]),2) 
-        best_tuple = (np.inf,0,0) 
+        best_tuple = (-1*np.inf,0,0)  #dummy cost variable meant to represent the extreme of the cost 
         best_pair = (0,0)
         unique_list = self.generate_unique_values(X)
-        for f1,f2 in combi:  
+        for f1,f2 in combi:   #we iterate thorugh all possible combinations features
             current_cost  = self.eval_metrix(X[:,[f1,f2]],Y,unique_list[f1],unique_list[f2])  #shape of currenct_cos (c_cost,f1_t,f2_t) 
-            if current_cost[0] < best_tuple[0]:
+            if current_cost[0] > best_tuple[0]:
                 best_tuple = current_cost 
                 best_pair = (f1,f2) 
         return  (best_tuple[0],best_pair[0],best_pair[1],best_tuple[1],best_tuple[2])
     def eval_metrix(self,x,y,f1_unique,f2_unique): 
+        """ eval_metrx: given a set of featur epairs find the best threshold split 
+
+        """
         feat_pairs = list(product(f1_unique,f2_unique)  ) #let's look at every combination  of these feature values    
-        best_tuple = (np.inf,0,0)
+        best_tuple = (-1*np.inf,0,0)
         for f1,f2 in feat_pairs: 
             current_cost = self.eval_cost(x,y,f1,f2) # shape of current loss (LL,f1_val,f2_val) 
-            if current_cost[0] < best_tuple[0] :
+            if current_cost[0] > best_tuple[0] :
                 best_tuple = current_cost  
         return best_tuple
     def inbetween_vals(self,whole_x): 
@@ -85,7 +102,6 @@ class decisionTree:
         if self.num_sample: 
             u_vals  = np.unique(whole_x) 
             potential_vals = np.zeros((self.num_sample,1))
-            min_u = min(u_vals) 
             counter = 0
             for i in range(0,len(u_vals),math.ceil(len(u_vals)/self.num_sample) ): 
                 potential_vals[counter] =  u_vals[i] 
@@ -99,8 +115,7 @@ class decisionTree:
 
     def eval_cost(self,x,y,f1_val,f2_val):
         """ since this is a 4-way decision problem. we can no longer asign onse side is negativ eone side is positive. 
-            we may wish to utilize the  most common class in each of the corresponding buckets as our prediciton label 
-            this would lead us to the potential if lacking proper class splits producing a null classifier
+            We thefore measure purity based on the positive class alone. 
         """
         l_f1 = x[:,0] <= f1_val 
         l_f2 = x[:,1] <= f2_val 
@@ -112,10 +127,13 @@ class decisionTree:
             my_LL = self.get_bucket_performance(y[bucket]) 
             LL += my_LL 
         if LL == math.inf or math.isnan(LL): 
-            return (math.inf,f1_val,f2_val) 
+            return (-1*math.inf,f1_val,f2_val) 
         else: 
             return (LL,f1_val,f2_val)
     def get_bucket_performance(self,real_labels):  
+        """
+            used to measure the homogeneity of an individual split 
+        """
         if real_labels.size != 0:  
             p = np.mean(real_labels)
             out1 = -1*p*np.log(p) 
@@ -124,17 +142,16 @@ class decisionTree:
                 out1 = 0  
             if out2 == -1*math.inf: 
                 out2 = 0 
-            return real_labels.size*(out1 + out2)
+            return -1*real_labels.size*(out1 + out2)
         else:
-            return 0
-class DaRDecisionTree(decisionTree):
+            return -1*math.inf
+class DaRDecisionTree(QuarternaryDecisionTree):
     def __init__(self,depth=2):  
         self.depth = depth 
         self.decision =0 
         self.children=None
     def fit(self,x,y):
-        t = time.time() 
-        if self.depth ==1:  
+        if self.depth ==0:  
             #handle case where we've reached the bottom 
             self.decision =  Ridge()
             self.decision.fit(x,y)
@@ -144,11 +161,10 @@ class DaRDecisionTree(decisionTree):
             self.decision = None 
             #case where we are not the bottom and need to split 
             (cost,f1,f1_val) = self.get_best(x,y) 
-            print(f"With cost {cost} we found f1: {f1} t: {f1_val}")
             #set up decision variables 
             self.feat_1 = f1 
             self.feat_1_thresh = f1_val 
-            l_f1 = x[:,f1] < f1_val 
+            l_f1 = x[:,f1] <= f1_val 
             g_f1 =  np.logical_not(l_f1) 
             buckets = [l_f1,g_f1] 
             for b in buckets:  
@@ -165,14 +181,10 @@ class DaRDecisionTree(decisionTree):
             for i,b in enumerate(buckets): 
                 if b:
                     return  self.children[i].predict(x) 
-
-    
     def get_best(self,X,Y):  
         """ in this case were only splititng on a single variable 
             only iterate through a single list 
-
         """
-        t = time.time()
         best_tuple = (np.inf,0) 
         best_feat = 0
         unique_list = self.generate_unique_values(X)
@@ -181,16 +193,13 @@ class DaRDecisionTree(decisionTree):
             if current_cost[0] < best_tuple[0]:
                 best_tuple = current_cost 
                 best_feat = f1 
-            #print(f"We finished evaluating one feature set out of{X.shape[1]} time: {time.time() - t}")
         return  (best_tuple[0], best_feat, best_tuple[1] )
     def eval_metrix(self,x,y,f1_unique): 
-        t = time.time()
         best_tuple = (np.inf,0,0)
         for thresh_candidate in f1_unique :
             current_cost = self.eval_cost(x,y,thresh_candidate) # shape of current loss (LL,f1_val) 
             if current_cost[0] < best_tuple[0]:
                 best_tuple = current_cost   
-        #print(f"Finished splits for a feature cost{best_tuple[0]} time:{time.time() - t} ")
         return best_tuple
     def inbetween_vals(self,whole_x): 
         """ inbetween_vals generates thresholds based on middle point between unique values  
@@ -202,7 +211,6 @@ class DaRDecisionTree(decisionTree):
         for i in range(len(potential_vals) -1): 
             inbetween.append( (potential_vals[i] + potential_vals[i+1] )/2)   
         return inbetween
-
     def eval_cost(self,x,y,f1_val):
         """ since this is a 4-way decision problem. we can no longer asign onse side is negativ eone side is positive. 
             we may wish to utilize the  most common class in each of the corresponding buckets as our prediciton label 
@@ -216,7 +224,7 @@ class DaRDecisionTree(decisionTree):
             my_LL = self.get_bucket_performance(y[bucket]) 
             LL += my_LL 
         if LL == math.inf or math.isnan(LL): 
-            return (1*math.inf,f1_val) 
+            return (math.inf,f1_val) 
         else: 
             return (LL,f1_val)
     def get_bucket_performance(self,real_labels):  
@@ -224,5 +232,3 @@ class DaRDecisionTree(decisionTree):
             return real_labels.size*np.var(real_labels)
         else:
             return 0
-
-
